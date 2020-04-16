16679323dad980520f6571bba05dc2c6bb882dfd
199692sulei <13271325338.com>
2020-04-16 19:53:18 +0800

    2-5

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StudentList from "./conponents/StudentList"
-const appkey="13271325338_1569760343166"
-async function findAllStudents() {
-    const stus=await fetch("https://open.duyiedu.com/api/student/findAll?appkey="+appkey)
-    .then(resp=>resp.json()).then(resp=>resp.data)
-     return stus
-}
-async function render(params) {
-     ReactDOM.render( '正在加载中.....' , document.getElementById('root'));
-    const stus=await findAllStudents()
-    ReactDOM.render( <StudentList stus={stus} /> , document.getElementById('root'));
-}
-render()
+// import Tick from "./conponents/Tick"
+import A from "./conponents/Test"
+
+// let num=10
+
+// ReactDOM.render(<Tick number={num}/>  , document.getElementById('root'));
+ReactDOM.render(<A/>  , document.getElementById('root'));
+
+
 
 
