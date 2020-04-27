b207f65ae345c8c67f051f1298bed10de6f4ec3d
199692sulei <13271325338.com>
2020-04-27 18:03:04 +0800

    三栏布局

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from "./components/Test"
-ReactDOM.render( <Test></Test> , document.getElementById('root'));
+import App from "./App"
+ReactDOM.render( <App/>, document.getElementById('root'));
 
 
