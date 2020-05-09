d487ae69e8187df993be3ca0e4feb4b26e4f2109
199692sulei <13271325338.com>
2020-05-09 14:50:15 +0800

    3-5ref

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import Test from "./components/common/SelectBoxGroup/Test"
-// import {A,B} from "./components/Comps"
-// import withLog from "./HOC/withLog"
-// import withLogin from "./HOC/withLogin"
-// const AComp=withLogin(withLog(A,"ahg"))
-// const BComp=withLogin(withLog(B,"agh"))
+import Comp from "./components/Comp"
 export default function App() {
     return (
         <div>
-           {/* <AComp isLogin a={1}/>
-           <BComp isLogin b={2}/> */
-           <Test/>
-           }
+         <Comp/>
         </div>
     )
 }
---
+++ src/components/Comp.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class Comp extends Component {
+    state={
+        show:true
+    }
+    constructor(props){
+        super(props)
+        this.txt=React.createRef();
+    }
+    handleClick=e=>{
+        // console.log(this.txt)
+        // this.txt.focus()
+        this.setState({})
+        this.setState({
+            show:!this.state.show
+        })
+    }
+    componentDidMount(){
+        console.log("didMount",this.txt)
+    }
+    getRef=el=>{
+        console.log("函数被调用了",el)
+        this.txt=el
+    }
+    render() {
+        return (
+            <div>
+                    {this.state.show&& <input ref={this.getRef} type="text"/>}
+                  
+                    <button onClick={this.handleClick}>显示/隐藏</button>
+            </div>
+        )
+    }
+}
